8451076ff3fc61f550206fe882e25392659adb0b
DavidKasprak <kasprakdavid@gmail.com>
2024-08-13 23:43:27 +0300

    Homework 4 (started)

---
+++ Homework/Lesson-4/functions-part-1/task-1.js
@@ -0,0 +1,6 @@
+function rectangle (sideA, sideB) {
+    return sideA * sideB;
+}
+
+let result = rectangle(10, 5);
+console.log(result);
---
+++ Homework/Lesson-4/functions-part-1/task-2.js
@@ -0,0 +1,6 @@
+function circle (radiusNum) {
+    return 3.14 * radiusNum * radiusNum;
+}
+
+let result = circle(10)
+console.log(result)
